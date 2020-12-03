c3adfde8c3762d9b4618e95babdac012c43cd669
vijay <vijaybalajitheboss@gmail.com>
2020-12-03 11:52:07 +0530

    Completed SelectionSort

---
+++ src/Modules/SelectionSort.py
@@ -0,0 +1,16 @@
+from Modules.Swap import swap
+
+def selectionsort(array):
+    if len(array) == 1:
+        return
+
+    for i in range(len(array)):
+        min_val = array[i]
+        min_index = i
+        for j in range(i, len(array)):
+            if array[j] < min_val:
+                min_val = array[j]
+                min_index = j
+            yield array
+        swap(array, i, min_index)
+        yield array
