8a829629bb8101b15e1cc579b56280ae8fc87b7e
vijay <vijaybalajitheboss@gmail.com>
2020-12-03 11:26:24 +0530

    Added MergeSort algorithm

---
+++ src/Modules/MergeSort.py
@@ -0,0 +1,35 @@
+def mergesort(array, low, high):
+
+    if high <= low:
+        return
+
+    mid = low + ((high - low + 1) // 2) - 1
+    yield from mergesort(array, low, mid)
+    yield from mergesort(array, mid + 1, high)
+    yield from merge(array, low, mid, high)
+    yield array
+
+def merge(A, start, mid, end):
+    merged = []
+    left_index = start
+    right_index = mid + 1
+
+    while left_index <= mid and right_index <= end:
+        if A[left_index] < A[right_index]:
+            merged.append(A[left_index])
+            left_index = left_index + 1
+        else:
+            merged.append(A[right_index])
+            right_index = right_index + 1
+
+    while left_index <= mid:
+        merged.append(A[left_index])
+        left_index = left_index + 1
+
+    while right_index <= end:
+        merged.append(A[right_index])
+        right_index = right_index + 1
+
+    for i, sorted_val in enumerate(merged):
+        A[start + i] = sorted_val
+        yield A
