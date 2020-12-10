98ce9e04352f7fc2a336bcefa42ac61071027aab
vijay <vijaybalajitheboss@gmail.com>
2020-12-10 11:15:14 +0530

    Minor indentation changes

--- src/Modules/Csv_writer.py
+++ src/Modules/Csv_writer.py
@@ -3,7 +3,7 @@ from Modules.Colours import *
 
 def write(title,data,count):
 
-    temp = " "
+    temp = " "  
     for x in data:
         temp = temp + str(x) + " "
 
@@ -12,9 +12,9 @@ def write(title,data,count):
     try:
         open("Sorting_algorithm_data.csv","r")
     except:
-         with open("Sorting_algorithm_data.csv","a+") as csvfile:
-             writer = csv.DictWriter(csvfile, fieldnames = fields)
-             writer.writeheader()
+        with open("Sorting_algorithm_data.csv","a+") as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames = fields)
+            writer.writeheader()
 
     with open("Sorting_algorithm_data.csv","a+") as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames = fields) 
